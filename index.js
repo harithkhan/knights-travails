65bc18b33616d19defe966d20082c22a6479c05c
Harith Khan <184179594+harithkhan@users.noreply.github.com>
2025-02-19 14:37:51 +0800

    feat: find first possible knight traversal

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 import { knightMoves } from "./knight-moves.js";
+import { exploreKnightMoves } from "./knight-moves.js";
 
 console.log(knightMoves([0, 0], [0, 1]));
+console.log(exploreKnightMoves([0, 0]));
--- knight-moves.js
+++ knight-moves.js
@@ -1,6 +1,8 @@
-// exploreKnightMoves(coordinates) {
-
-// }
+export function exploreKnightMoves(coordinates) {
+    const possibleNewCoordinates = [];
+    const newCoordinates1 = [coordinates[0] + 1, coordinates[1] + 2];
+    console.log(newCoordinates1);
+}
 
 export function knightMoves(startCoordinates, targetCoordinates) {
     if (
@@ -11,5 +13,5 @@ export function knightMoves(startCoordinates, targetCoordinates) {
     }
     const queue = [startCoordinates];
     const visitedGraph = new Map();
-    console.log(visitedGraph);
+    return "failed";
 }
