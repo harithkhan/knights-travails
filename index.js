24af121b6a2566f2b6313eb3e87019b2f94a41ba
Harith Khan <184179594+harithkhan@users.noreply.github.com>
2025-02-19 14:59:33 +0800

    feat: find only legal first knight moves

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ import { knightMoves } from "./knight-moves.js";
 import { exploreKnightMoves } from "./knight-moves.js";
 
 console.log(knightMoves([0, 0], [0, 1]));
-console.log(exploreKnightMoves([4, 4]));
+console.log(exploreKnightMoves([0, 0]));
--- knight-moves.js
+++ knight-moves.js
@@ -1,4 +1,4 @@
-export function exploreKnightMoves(coordinates) {
+function findNewCoordinates(coordinates) {
     const possibleNewCoordinates = [];
     const newCoordinates1 = [coordinates[0] + 1, coordinates[1] + 2];
     const newCoordinates2 = [coordinates[0] + 2, coordinates[1] + 1];
@@ -8,15 +8,77 @@ export function exploreKnightMoves(coordinates) {
     const newCoordinates6 = [coordinates[0] - 2, coordinates[1] - 1];
     const newCoordinates7 = [coordinates[0] - 2, coordinates[1] + 1];
     const newCoordinates8 = [coordinates[0] - 1, coordinates[1] + 2];
+    if (
+        newCoordinates1[0] >= 0 &&
+        newCoordinates1[0] <= 7 &&
+        newCoordinates1[1] >= 0 &&
+        newCoordinates1[1] <= 7
+    ) {
+        possibleNewCoordinates.push(newCoordinates1);
+    }
+    if (
+        newCoordinates2[0] >= 0 &&
+        newCoordinates2[0] <= 7 &&
+        newCoordinates2[1] >= 0 &&
+        newCoordinates2[1] <= 7
+    ) {
+        possibleNewCoordinates.push(newCoordinates2);
+    }
+    if (
+        newCoordinates3[0] >= 0 &&
+        newCoordinates3[0] <= 7 &&
+        newCoordinates3[1] >= 0 &&
+        newCoordinates3[1] <= 7
+    ) {
+        possibleNewCoordinates.push(newCoordinates3);
+    }
+    if (
+        newCoordinates4[0] >= 0 &&
+        newCoordinates4[0] <= 7 &&
+        newCoordinates4[1] >= 0 &&
+        newCoordinates4[1] <= 7
+    ) {
+        possibleNewCoordinates.push(newCoordinates4);
+    }
+    if (
+        newCoordinates5[0] >= 0 &&
+        newCoordinates5[0] <= 7 &&
+        newCoordinates5[1] >= 0 &&
+        newCoordinates5[1] <= 7
+    ) {
+        possibleNewCoordinates.push(newCoordinates5);
+    }
+    if (
+        newCoordinates6[0] >= 0 &&
+        newCoordinates6[0] <= 7 &&
+        newCoordinates6[1] >= 0 &&
+        newCoordinates6[1] <= 7
+    ) {
+        possibleNewCoordinates.push(newCoordinates6);
+    }
+    if (
+        newCoordinates7[0] >= 0 &&
+        newCoordinates7[0] <= 7 &&
+        newCoordinates7[1] >= 0 &&
+        newCoordinates7[1] <= 7
+    ) {
+        possibleNewCoordinates.push(newCoordinates7);
+    }
+    if (
+        newCoordinates8[0] >= 0 &&
+        newCoordinates8[0] <= 7 &&
+        newCoordinates8[1] >= 0 &&
+        newCoordinates8[1] <= 7
+    ) {
+        possibleNewCoordinates.push(newCoordinates8);
+    }
+    return possibleNewCoordinates;
+}
 
-    console.log(newCoordinates1);
-    console.log(newCoordinates2);
-    console.log(newCoordinates3);
-    console.log(newCoordinates4);
-    console.log(newCoordinates5);
-    console.log(newCoordinates6);
-    console.log(newCoordinates7);
-    console.log(newCoordinates8);
+export function exploreKnightMoves(coordinates) {
+    const possibleNewCoordinates = findNewCoordinates(coordinates);
+    return possibleNewCoordinates;
+
 }
 
 export function knightMoves(startCoordinates, targetCoordinates) {
