a363423f54439b11adf695b4bc1df0f1cec5196c
Harith Khan <184179594+harithkhan@users.noreply.github.com>
2025-02-19 14:46:33 +0800

    feat: find possible first knight moves

--- knight-moves.js
+++ knight-moves.js
@@ -1,7 +1,22 @@
 export function exploreKnightMoves(coordinates) {
     const possibleNewCoordinates = [];
     const newCoordinates1 = [coordinates[0] + 1, coordinates[1] + 2];
+    const newCoordinates2 = [coordinates[0] + 2, coordinates[1] + 1];
+    const newCoordinates3 = [coordinates[0] + 2, coordinates[1] - 1];
+    const newCoordinates4 = [coordinates[0] + 1, coordinates[1] - 2];
+    const newCoordinates5 = [coordinates[0] - 1, coordinates[1] - 2];
+    const newCoordinates6 = [coordinates[0] - 2, coordinates[1] - 1];
+    const newCoordinates7 = [coordinates[0] - 2, coordinates[1] + 1];
+    const newCoordinates8 = [coordinates[0] - 1, coordinates[1] + 2];
+
     console.log(newCoordinates1);
+    console.log(newCoordinates2);
+    console.log(newCoordinates3);
+    console.log(newCoordinates4);
+    console.log(newCoordinates5);
+    console.log(newCoordinates6);
+    console.log(newCoordinates7);
+    console.log(newCoordinates8);
 }
 
 export function knightMoves(startCoordinates, targetCoordinates) {
