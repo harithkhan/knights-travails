ef9795f32e69ffaf39027bf53eecf081a82bb8c5
Harith Khan <184179594+harithkhan@users.noreply.github.com>
2025-02-19 14:29:03 +0800

    feat: initialize queue and visitedGraph

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 import { knightMoves } from "./knight-moves.js";
 
-console.log(knightMoves([0, 0], [0, 0]));
+console.log(knightMoves([0, 0], [0, 1]));
--- knight-moves.js
+++ knight-moves.js
@@ -1,11 +1,15 @@
+// exploreKnightMoves(coordinates) {
+
+// }
+
 export function knightMoves(startCoordinates, targetCoordinates) {
     if (
         startCoordinates[0] === targetCoordinates[0] &&
-        startCoordinates[0] === targetCoordinates[0]
+        startCoordinates[1] === targetCoordinates[1]
     ) {
         return `Reached ${startCoordinates} in 0 moves`;
     }
-    return "failed";
-    // const queue = [startCoordinates];
-    // const visitedGraph = new Map();
+    const queue = [startCoordinates];
+    const visitedGraph = new Map();
+    console.log(visitedGraph);
 }
