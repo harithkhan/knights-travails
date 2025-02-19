386d95536999ff5e734eafdc09b735c4360f686c
Harith Khan <184179594+harithkhan@users.noreply.github.com>
2025-02-19 18:49:07 +0800

    fix: remove exploreKnightMoves(), it was rubbish

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
 import { knightMoves } from "./knight-moves.js";
-import { exploreKnightMoves } from "./knight-moves.js";
 
 console.log(knightMoves([0, 0], [0, 1]));
-console.log(exploreKnightMoves([0, 0]));
--- knight-moves.js
+++ knight-moves.js
@@ -75,19 +75,8 @@ function findNewCoordinates(coordinates) {
     return possibleNewCoordinates;
 }
 
-export function exploreKnightMoves(coordinates) {
-    const possibleNewCoordinates = findNewCoordinates(coordinates);
-    return possibleNewCoordinates;
-
-}
-
 export function knightMoves(startCoordinates, targetCoordinates) {
-    if (
-        startCoordinates[0] === targetCoordinates[0] &&
-        startCoordinates[1] === targetCoordinates[1]
-    ) {
-        return `Reached ${startCoordinates} in 0 moves`;
-    }
+
     const queue = [startCoordinates];
     const visitedGraph = new Map();
     return "failed";
